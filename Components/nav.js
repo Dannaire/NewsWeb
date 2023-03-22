185ae5004f54bc78af94db15598d97f21bbffa1e
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2023-03-23 05:09:04 +0700

    update Navbar

--- Components/nav.js
+++ Components/nav.js
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
-// import Utama from './pages/utama'
-import { Transition } from "@headlessui/react";
+import React from "react";
 import Link from "next/link";
 
 function Nav() {
-  //   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <nav className="bg-navbcg border-gray-200 dark:bg-gray-900 h-20 w-full fixed">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-          <Link href="https://flowbite.com" class="flex items-center">
+          <Link href="/" class="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               class="h-6 mr-3 sm:h-9"
@@ -20,11 +17,11 @@ function Nav() {
             </span>
           </Link>
           <div className="flex items-center">
-            <ul className="flex flex-row my-5 mr-6 space-x-8 text-sm font-medium">
+            <ul className="flex flex-row my-5 mr-6 space-x-8 text-sm font-medium ">
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                   aria-current="page"
                 >
                   Home
@@ -32,44 +29,44 @@ function Nav() {
               </li>
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/business"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                 >
                   Business
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/sport"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                 >
                   Sport
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/health"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                 >
                   Health
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/travel"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                 >
-                  Politics
+                  Travel
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline duration-300"
+                  href="/world"
+                  className="border-b-2 pb-[2px] border-transparent text-gray-900 dark:text-white hover:border-white duration-300"
                 >
                   World
                 </Link>
-              </li>
+              </li> 
             </ul>
           </div>
         </div>
