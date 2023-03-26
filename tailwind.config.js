d996acf0330c3c44bca6a4e05a5c42e2fdcf6ad3
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-03-26 22:21:09 +0700

    fix vercel errorr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 
 module.exports = {
+  enabled: process.env.NODE_ENV === "production",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,7 +9,15 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: [ "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ],
+  
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
+
   theme: {
     colors: {
       transparent: 'transparent',
