58c7835e8f29ae341b458d0ff75260567e7c4b5a
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-03-26 13:00:02 +0700

    adjust detail

--- pages/detail/[resultId].js
+++ pages/detail/[resultId].js
@@ -29,7 +29,7 @@ export default function Detail() {
           </div>
         </div>
         {/* block news */}
-        <div className="bg-gray-50 py-12">
+        <div className="bg-gray-50 py-24">
           <div className="xl:container mx-auto px-3 sm:px-4 xl:px-12">
             <div className="flex flex-row flex-wrap">
               {/* Left */}
@@ -52,7 +52,6 @@ export default function Detail() {
                       </figure>
                       <h3 className="text-2xl leading-normal mb-2 font-semibold text-gray-800 dark:text-gray-100">Berita Hari Ini</h3>
                       <p className="mb-5">{result.conten}</p>
-                      <p className="mb-5">Integer egestas ipsum eget metus sodales consectetur. Nullam ultricies posuere cursus. Duis vitae lorem porta, venenatis nibh ac, laoreet massa. Nam risus lacus, porta eu diam id, fringilla porta risus. Aenean sit amet malesuada diam.</p>
                       <blockquote className="relative p-4 border-l-4 border-red-700 bg-gray-100 dark:bg-gray-900 dark:bg-opacity-40 mb-4 text-xl">
                         <span className="absolute opacity-80 w-8 h-8">
                           <svg xmlns="http://www.w3.org/2000/svg" className="text-red-700" viewBox="0 0 270.000000 270.000000"><g transform="translate(0.000000,270.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none"><path d="M920 2182 c-290 -124 -482 -341 -540 -610 -30 -140 -40 -296 -40 -644 l0 -328 370 0 370 0 0 370 0 370 -181 0 -181 0 7 63 c26 243 129 387 342 477 35 15 66 29 69 32 7 7 -132 298 -143 298 -4 0 -37 -13 -73 -28z" /><path d="M2179 2186 c-249 -103 -442 -295 -520 -516 -50 -142 -61 -247 -66 -677 l-5 -393 371 0 371 0 0 370 0 370 -181 0 -181 0 7 53 c21 170 67 281 150 363 51 49 143 107 215 134 19 7 39 17 44 21 10 9 -124 298 -139 298 -5 0 -35 -10 -66 -23z" /></g></svg>
@@ -60,26 +59,7 @@ export default function Detail() {
                         <p className="ml-16 mb-4">"News is what somebody somewhere wants to suppress; all the rest is advertising."</p>
                         <footer className="ml-16 text-base">Quote by <cite title="Source Title">Lord Northcliffe</cite></footer>
                       </blockquote>
-                      <p className="mb-5">Vivamus purus orci, molestie vel erat sed, consectetur posuere ligula. Vestibulum <br /> iaculis dignissim laoreet. Cras tincidunt always have Paris, at lobortis ligula laoreet. Etiam eu sapien sit amet neque aliquam consequat nec in velit. Aliquam sit amet erat sed augue bibendum vehicula non at lacus. Phasellus scelerisque in elit a els.</p>
-                      <p className="mb-5">Cras justo velit, ultrices vel vehicula eu, viverra in turpis. Donec lobortis at lorem ac semper. Mauris malesuada ligula in interdum pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                      <figure className="lg:float-left text-center lg:text-left ml-0 lg:-ml-4 mr-7 mb-7">
-                        <img className="max-w-full h-auto mx-auto" src="src/img/dummy/post2.jpg" alt="Image description" />
-                        <figcaption>Align left images</figcaption>
-                      </figure>
-                      <p className="mb-5">Morbi at lacinia risus. Donec vitae justo sed augue sollicitudin dignissim ut vitae ipsum. Proin at imperdiet ante, non blandit mauris. Nullam eu lobortis justo.</p>
-                      <p className="mb-5">Fusce vestibulum blandit justo non rhoncus. Maecenas eget felis id orci ultricies frin gilla. Aliquam suscipit enim felis. Praesent arcu dui, rutrum et molestie sed, volutpat auctor lorem. Fusce sit amet libero odio. Integer sed nisl turpis. Integer mollis sed est eget convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-                      <p className="mb-5">Fusce elementum placerat tellus id. Nulla sit amet pretium enim, in vehicula ligula. Proin nec malesuada liberoque blandit. Sed condimentum neque ligula, id dapibus enim ornare id. Duis porttitor, risus vehicula convallis sagittis, ligula nisi iaculis libero, sit amet convallis nulla magna non tortor.</p>
-                      <h2 className="float-left w-full text-xl leading-normal mb-2 font-semibold text-gray-800 dark:text-gray-100">Live as if you were to die tomorrow.</h2>
-                      <p className="mb-5">Nullam ut tempus mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus libero dolor, condimentum eu augue et, placerat tempus risus. Quisque massa purus, ullamcorper eget ipsum et, eleifend suscipit mi. Duis auctor tortor a dui accumsan, sed semper nunc iaculis.</p>
-                      <figure className="lg:float-right text-center lg:text-right mr-0 lg:-mr-4 ml-7 mb-7">
-                        <img className="max-w-full h-auto mx-auto" src="img/aqsa.jpeg" alt="Image description" />
-                        <figcaption>Align right images</figcaption>
-                      </figure>
-                      <p className="mb-5">Mauris eget lectus et nisi commodo tristique. Aenean eget ornare dui. Maecenas bibendum lacus ante, vitae pellentesque ex elementum convallis.</p>
-                      <p className="mb-5">Mauris lobortis leo a neque congue, quis pretium libero efficitur. Sed congue ipsum ac mollis egestas. Sed venenatis nisl nibh, in blandit nisi varius sed. Pellentesque molestie lacus ut justo molestie tristique. Curabitur pretium ipsum ex, a rhoncus arcu tempus ut. Fusce elementum placerat tellus id blandit.</p>
-                      <p className="mb-5">Sed condimentum neque ligula, id dapibus enim ornare id. Duis porttitor, risus vehicula convallis sagittis, ligula nisi iaculis libero.</p>
-                      <p className="mb-5">Morbi at lacinia risus. Donec vitae justo sed augue sollicitudin dignissim ut vitae ipsum. Proin at imperdiet ante, non blandit mauris. Nullam eu lobortis justo.</p>
-                      <p className="mb-5">Fusce vestibulum blandit justo non rhoncus. Maecenas eget felis id orci ultricies frin gilla. Aliquam suscipit enim felis. Praesent arcu dui, rutrum et molestie sed, volutpat auctor lorem. Fusce sit amet libero odio. Integer sed nisl turpis. Integer mollis sed est eget convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                     
                       <div className="relative flex flex-row items-center justify-between overflow-hidden bg-gray-100 dark:bg-gray-900 dark:bg-opacity-20 mt-12 mb-2 px-6 py-2">
                         <div className="my-4 text-sm">
                           {/*author*/}
