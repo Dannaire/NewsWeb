d5ef6efd15fea33ec2d0923c2103f5494aa1590e
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-03-28 11:35:55 +0700

    add color

--- Components/nav.js
+++ Components/nav.js
@@ -11,7 +11,7 @@ function Nav() {
   return (
     <div>
       <nav
-        className="bg-navbcg border-gray-200 dark:bg-gray-900 h-20 w-full fixed z-10" 
+        className="bg-navbcg border-gray-200 h-20 w-full fixed z-10" 
       >
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
           <Link href="/" class="flex items-center">
